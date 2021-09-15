242a5b174a3f748bd4c3537c4598f95e9daf6de2
tanktejas <tejastank20001@gmail.com>
2021-09-15 10:09:40 +0530

    asa

--- a.js
+++ a.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const exp = express();
+const path = require('path');
+// console.log(__dirname);
+const path1 = path.join(__dirname, 'views/a.html');
+// console.log(path1);  
+// exp.use(express.static(path1));
+// exp.set('view engine', 'ejs');
+// exp.set('views', path.join(__dirname, 'views'));
+
 const port = process.env.PORT || 3000;
 
+const path2 = path.join(__dirname, './views');
+
+exp.use(express.static(path2));
+  
 exp.get('/', (req, res) => {
-    res.send("hello from home side");
+    res.sendFile(path1);
+});
+exp.get('/b.html', (req, res) => {
+    res.sendFile(path2);
 });
 
 exp.listen(port, () => {
